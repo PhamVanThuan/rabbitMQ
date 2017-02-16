3edb5f493c95ebb3a9cdce5802f18d9308088f4b
simplymarci <guidi.marcello@gmail.com>
2017-02-16 12:41:27 +0100

    ignore working actually g

---
+++ WorkQueues/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace WorkQueues
+{
+    class Program
+    {
+        private static void Main()
+        {
+            var sendingDone = true;
+            var receivingDone = true;
+
+            Parallel.Invoke(
+                () => { sendingDone = new QueueWriter(WorkQueuesConfig.MessageNo, WorkQueuesConfig.QueueName).writeOnQueue(); },
+                () => { receivingDone = new QueueReader("1st", WorkQueuesConfig.MessageNo, WorkQueuesConfig.QueueName).ReadFromQueue(); },
+                () => { receivingDone = new QueueReader("2nd", WorkQueuesConfig.MessageNo, WorkQueuesConfig.QueueName).ReadFromQueue(); },
+                () => { receivingDone = new QueueReader("3rd", WorkQueuesConfig.MessageNo, WorkQueuesConfig.QueueName).ReadFromQueue(); },
+                () => { receivingDone = new QueueReader("4th", WorkQueuesConfig.MessageNo, WorkQueuesConfig.QueueName).ReadFromQueue(); },
+                () => { receivingDone = new QueueReader("5th", WorkQueuesConfig.MessageNo, WorkQueuesConfig.QueueName).ReadFromQueue(); }
+                );
+
+            while (sendingDone || receivingDone) { }
+
+            Console.WriteLine("");
+            Console.WriteLine("Job Complete!");
+            Console.ReadLine();
+        }
+    }
+}
+           Console.WriteLine("Job Complete!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WorkQueues/QueueReader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Entitites;
+
+namespace WorkQueues
+{
+    class QueueReader
+    {
+        public QueueReader(string readerIdentifier, int messageNo, string queueName)
+        {
+            ReaderIdentifier = readerIdentifier;
+            MessageNo = messageNo;
+            QueueName = queueName;
+            Consumer = new Consumer(WorkQueuesConfig.Host, WorkQueuesConfig.QueueName);
+            Consumer.OnMessageReceived += HandleMessage;
+        }
+
+        private Consumer Consumer { get; set; }
+        private string ReaderIdentifier { get; set; }
+        private int MessageNo { get; set; }
+        private string QueueName { get; set; }
+
+        public bool ReadFromQueue()
+        {
+            Consumer.StartConsuming();
+            return true;
+        }
+
+        private void HandleMessage(byte[] queueMessage)
+        {
+            var message = Encoding.UTF8.GetString(queueMessage);
+            Console.WriteLine(" [ Receiving message by {0}: {1} ]", ReaderIdentifier, message);
+        }
+    }
+}
