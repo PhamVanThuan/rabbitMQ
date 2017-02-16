229c571facb6de3ce454f14566e50348be07ea29
simplymarci <guidi.marcello@gmail.com>
2017-02-16 12:39:47 +0100

    .gitignore is now working

--- HelloWorld/Config.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HelloWorld
-{
-    public static class Config
-    {
-        public static readonly int MessageNo = 1000;
-        public static readonly string QueueName = "HELLO_QUEUE";
-        public const string Host = "localhost";
-    }
-}
--- WorkQueues/WorkQueuesConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WorkQueues
-{
-    public static class WorkQueuesConfig
-    {
-        public const int MessageNo = 10000;
-        public const string QueueName = "TASK_QUEUE";
-        public const string Host = "localhost";
-    }
-}
